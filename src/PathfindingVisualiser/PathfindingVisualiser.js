e4de62925c2feb32e7db18f605cc59bf82c30df4
tommyrharper <tommyrharper@gmail.com>
2020-06-17 20:13:02 +0100

    Create Pathfinding component

---
+++ src/PathfindingVisualiser/PathfindingVisualiser.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class PathfindingVisualiser extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default PathfindingVisualiser
