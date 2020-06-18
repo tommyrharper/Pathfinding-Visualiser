b56fc95b4a06a79489f8c73edca59b45f2877daf
tommyrharper <tommyrharper@gmail.com>
2020-06-18 10:46:45 +0100

    Update Visualiser component to include node

--- src/PathfindingVisualiser/PathfindingVisualiser.js
+++ src/PathfindingVisualiser/PathfindingVisualiser.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import Node from './Node/Node'
 
-export class PathfindingVisualiser extends Component {
+export default class PathfindingVisualiser extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
   render() {
     return (
       <div>
+        <Node></Node>
       </div>
     )
   }
 }
-
-export default PathfindingVisualiser
