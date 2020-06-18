903b02a012a1afe811d8f85bcd96912753c04c86
tommyrharper <tommyrharper@gmail.com>
2020-06-18 11:33:51 +0100

    Add start and finish node to Visualiser

--- src/PathfindingVisualiser/PathfindingVisualiser.js
+++ src/PathfindingVisualiser/PathfindingVisualiser.js
@@ -16,7 +16,13 @@ export default class PathfindingVisualiser extends Component {
     for (let row = 0; row < 15; row++) {
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
@@ -31,9 +37,17 @@ export default class PathfindingVisualiser extends Component {
         {nodes.map((row, rowIdx) => {
           return (
             <div key={rowIdx}>
-              {row.map((node, nodeIdx) => (
-                <Node key={nodeIdx} isStart={true}></Node>
-              ))}
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                    test={"foo"}
+                  ></Node>
+                );
+              })}
             </div>
           );
         })}
