726788818f0642926993718b58fc3619c0bde092
tommyrharper <tommyrharper@gmail.com>
2020-06-18 14:08:42 +0100

    Update Node.js

--- src/PathfindingVisualiser/Node/Node.js
+++ src/PathfindingVisualiser/Node/Node.js
@@ -3,23 +3,33 @@ import React, { Component } from "react";
 import "./Node.css";
 
 export default class Node extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   render() {
-    const { isFinish, isStart } = this.props;
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
     const extraClassName = isFinish
       ? "node-finish"
       : isStart
       ? "node-start"
+      : isWall
+      ? "node-wall"
       : "";
 
-    return <div className={`node ${extraClassName}`}></div>;
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      ></div>
+    );
   }
 }
-
-export const DEFUALT_NODE = {
-  row: 0,
-  col: 0,
-};
