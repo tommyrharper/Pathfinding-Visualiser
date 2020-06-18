ceffad6545c91fd7f60bae9d34a8d3058d116a15
tommyrharper <tommyrharper@gmail.com>
2020-06-18 11:18:48 +0100

    Create Node grid within Visualiser

--- src/PathfindingVisualiser/PathfindingVisualiser.js
+++ src/PathfindingVisualiser/PathfindingVisualiser.js
@@ -6,13 +6,34 @@ import './PathfindingVisualser.css'
 export default class PathfindingVisualiser extends Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      nodes: []
+    };
+  }
+
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = []
+      for (let col = 0; col < 50; col++) {
+        currentRow.push([])
+      }
+      nodes.push(currentRow)
+    }
+    this.setState({nodes})
   }
   render() {
-    return (
-      <div>
-        <Node></Node>
+    const {nodes} = this.state
+    console.log(nodes)
+
+    return(
+      <div className="grid">
+        {nodes.map((row, rowIdx) => {
+          return <div>
+                  {row.map((node, nodeIdx) => <Node></Node>)}
+                </div>
+        })}
       </div>
-    )
+    );
   }
 }
