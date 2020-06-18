f5ab61e066db9f1f0567db4232fec183cbbfc097
tommyrharper <tommyrharper@gmail.com>
2020-06-18 13:54:49 +0100

    Build dijkstra .js half built

--- src/algorithms/Dijkstra.js
+++ src/algorithms/Dijkstra.js
@@ -1,44 +1,49 @@
-function dijkstra(
-  nodes,
-  start,
-  target,
-  nodesToAnimate,
-  boardArray,
-  name,
-  heuristsic
-) {
-  if (name === "astar")
-    return astar(nodes, start, target, nodesToAnimate, boardArray, name);
-  if (!start || !target || start === target) {
+const node = {
+  row,
+  col,
+  isVisited,
+  distance,
+};
+
+function dijkstra(grid, startnode, finishNode) {
+  if (!startNode || !finishNode || startNode === finishNode) {
     return false;
   }
-
-  nodes[start].distance = 0;
-  nodes[start].direction = "right";
-  let unvisitedNodes = Object.keys(nodes);
-  while (unvisitedNodes.length) {
-    let currentNode = closestNode(nodes, unvisitedNodes);
-    while (currentNode.status === "walll" && unvisitedNodes.length) {
-      currentNode = closestNode(nodes, unvisitedNodes);
-    }
-    if (currentNode.distance === Infinity) {
-      return false;
-    }
-    nodesToAnimate.push(currentNode);
-    currentNode.status = "visited";
-    if (currentNode.id === target) return "success!";
-    if (name === "CLA" || name === "greedy") {
-      updateNeighbours(
-        nodes,
-        currentNode,
-        boardArray,
-        target,
-        name,
-        start,
-        heuristic
-      );
-    } else if (name === "dijkstra") {
-      updateNeighbours(nodes, currentNode, boardArray);
-    }
+  nodes[startNode].distance = 0;
+  const unvisitedNodes = nodes.slice();
+  while (!!unvisitedNodes.length) {
+    sortNodesByDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.unshift();
+    // // HANDLE WALLS LATER
+    // while (currentNode.status === "wall" && unvisitedNodes.length) {
+    //   currentNode = getClosestNode(nodes, unvisitedNodes)
+    // }
+    // // HANDLE IMPOSSIBLE LATER
+    // if (closestNode.distance === Infinity) return false;
+    // // ANIMATE LATER
+    // nodesToAnimate.push(closestNode)
+    closestNode.isVisited = true;
+    if (closestNode === finishNode) return "success!";
+    updateNeighbors(closestNode, grid);
   }
+}
+
+function sortNodesByDistance(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+function updateNeighbors(node, grid) {
+  const neighbors = getNeighbors(node, grid);
+  for (const neighbor of neighbors) {
+    neighbor.distance = node.distance + 1;
+  }
+}
+
+function getNeighbors(node, grid) {
+  const neighbors = [];
+  const { col, row } = node;
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
 }
